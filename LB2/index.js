966ad784d3ec000f1f63b0bbdd32bfe249e0ee9f
Joel Vontobel <joel.vontobel@ergon.ch>
2023-06-08 13:03:10 +0000

    Added swagger.yaml and dependencies

--- LB2/index.js
+++ LB2/index.js
@@ -5,6 +5,16 @@ const tasks = require('/workspaces/m295LB2/LB2/controller/tasks.js');
 const auth = require('/workspaces/m295LB2/LB2/controller/authentication.js')
 const port = 3000;
 
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
+const YAML = require('yaml');
+
+const file = fs.readFileSync('./swagger.yaml', 'utf8');
+const swaggerDocument = YAML.parse(file);
+
+// URL for Swagger: http://localhost:3000/api-docs/#/
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(session({
